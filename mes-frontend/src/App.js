6fe57542e6c36bde88d7197f8944cdd82c4fcf47
cynthiaoduol <cynthiaobu940@gmail.com>
2020-01-18 21:50:05 +0300

    move the component files with the assessment forms to one folder

--- mes-frontend/src/App.js
+++ mes-frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MainAssessment from './components/MainAssessment'
+import MainAssessment from './components/Assessment/MainAssessment'
 
 function App() {
   return (
