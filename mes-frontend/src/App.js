c6da413b06df143a8e2f4090a5687bae239eaef1
cynthiaoduol <cynthiaobu940@gmail.com>
2020-01-18 18:29:48 +0300

    create employee assessment forms

--- mes-frontend/src/App.js
+++ mes-frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainAssessment from './components/MainAssessment'
 
 function App() {
   return (
-    <div className="App">
-    rrr
-    </div>
+
+    <MainAssessment></MainAssessment>
+    
   );
 }
 
